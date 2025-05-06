4a7a8b5d212c6a1a84e91a6760036fdf9682c5cd
Hani <139141380+Darkksoul@users.noreply.github.com>
2025-05-07 00:19:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ SUPPORT_CHAT_ID = -1001636707031
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URL = environ.get('DATABASE_URL', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "PIRO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
